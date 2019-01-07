ce9f026bc7d6cb1d315e9012f9508bb34fbfb7f6
张鹏（金山） <287206570@qq.com>
2019-01-07 18:58:08 +0800

    增加接口

--- worldnet/worldnet_if.go
+++ worldnet/worldnet_if.go
@@ -15,3 +15,11 @@ type IConnector interface {
 	Stop()            // 停止 connector
 	TypeName() string // connector 的类型(protocol.type)，例如tcp.Connector/udp.Acceptor
 }
+
+// Session 存取器接口
+type ISessionAccessor interface {
+	GetSession(int64) ISession        // 从 session 存取器中获取一个连接
+	VisitSession(func(ISession) bool) // 遍历连接
+	SessionCount() int                // 活跃的会话数量
+	CloseAllSession()                 // 关闭所有连接
+}
