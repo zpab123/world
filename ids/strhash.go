446a57c8f86febc4371dffaff4523a33f38eef64
张鹏（金山） <287206570@qq.com>
2019-01-08 14:15:28 +0800

    添加字符串ID生成方法

---
+++ ids/strhash.go
@@ -0,0 +1,17 @@
+// /////////////////////////////////////////////////////////////////////////////
+// 以利用字符串生成id
+
+package ids
+
+// 字符串转为16位整形值
+func StringHash(s string) (hash uint16) {
+
+	for _, c := range s {
+
+		ch := uint16(c)
+
+		hash = hash + ((hash) << 5) + ch + (ch << 7)
+	}
+
+	return
+}
