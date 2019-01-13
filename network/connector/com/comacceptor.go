96718a1337c217ed91121b7241b9337f7b88603f
zp_amd <287206570@qq.com>
2019-01-13 17:46:54 +0800

    优化代码

--- network/connector/com/comacceptor.go
+++ network/connector/com/comacceptor.go
@@ -5,12 +5,14 @@ package com
 
 import (
 	"net"
+	"net/http"
 	"strings"
 
-	"github.com/zpab123/world/model"             // 常用数据类型
+	"github.com/zpab123/world/model"             // 全局 [常量-基础数据类型-接口] 集合
 	"github.com/zpab123/world/network/connector" // 连接器
 	"github.com/zpab123/world/utils"             // 工具库
 	"github.com/zpab123/zplog"                   // 日志库
+	"golang.org/x/net/websocket"                 // websocket 库
 )
 
 // /////////////////////////////////////////////////////////////////////////////
@@ -31,6 +33,9 @@ type comAcceptor struct {
 	connector.SocketCreator                    // 对象继承： socket 创建管理
 	connector                 model.IConnector // connector 对象
 	tcpListener               net.Listener     // tcp 侦听器
+	wsListenAddr              string           // 监听成功的 websocket
+	certFile                  string           // TLS加密文件
+	keyFile                   string           // TLS解密key
 }
 
 // 创建1个 comAcceptor 对象
@@ -49,6 +54,7 @@ func (this *comAcceptor) Run() {
 	this.runTcpListener()
 
 	// 启动 websocket 侦听
+	this.runWsListener()
 }
 
 // 停止 Acceptor [IAcceptor 接口]
@@ -92,14 +98,14 @@ func (this *comAcceptor) runTcpListener() {
 
 	// 创建失败
 	if nil != err {
-		zplog.Errorf("启动 tcpAcceptor 失败。错误=%v", err.Error())
+		zplog.Errorf("tcpAcceptor-tcp 启动失败。错误=%v", err.Error())
 
 		return
 	}
 
 	// 创建成功
 	this.tcpListener = ln.(net.Listener)
-	zplog.Infof("comAcceptor-tcp 启动成功。监听地址=%s", this.GetTcpAddr())
+	zplog.Infof("comAcceptor-tcp 启动成功。ip=%s", this.GetTcpAddr())
 
 	// 侦听 tcp 连接
 	go this.acceptTcpConn()
@@ -109,7 +115,7 @@ func (this *comAcceptor) runTcpListener() {
 func (this *comAcceptor) acceptTcpConn() {
 	//  出现错误，关闭监听
 	closeF := func() {
-		zplog.Error("侦听 tcp 新连接出现错误。关闭 comAcceptor")
+		zplog.Error("comAcceptor 侦听 tcp 新连接出现错误。关闭 comAcceptor")
 		this.tcpListener.Close()
 	}
 	defer closeF()
@@ -140,6 +146,58 @@ func (this *comAcceptor) onNewTcpConn(conn net.Conn) {
 
 	// 通知 Connector 组件
 	this.connector.OnNewSocket(this.GetPacketSocket())
+}
+
+// 启动 websocket 侦听
+func (this *comAcceptor) runWsListener() {
+	// 变量定义
+	var (
+		addrObj *model.Address // 地址变量
+		wsPort  int            // 监听成功的 websocket 端口
+	)
+
+	// 查找1个 可用端口
+	f := func(addr *model.Address, port int) (interface{}, error) {
+		addrObj = addr
+		wsPort = port
+		return net.Listen("tcp", addr.HostPortString(port))
+	}
+
+	_, err := utils.DetectPort(this.GetAddr().WsAddr, f)
+
+	// 查找失败
+	if nil != err {
+		zplog.Errorf("comAcceptor-websocket 启动失败。错误=%v", err.Error())
+		return
+	}
+
+	// 端口查找成功
+	this.wsListenAddr = addrObj.String(wsPort)
+
+	// 设置 "/ws" 消息协议处理函数(客户端需要在url后面加上 /ws 路由)
+	http.Handle("/ws", websocket.Handler(this.onNewWsConn)) // 有新连接的时候，会调用 onNewWsConn 处理新连接
+
+	// 侦听新连接
+	go func() {
+		var err error
+		zplog.Infof("comAcceptor-websocket 启动成功。ip=%s", this.wsListenAddr)
+		if "" == this.keyFile && "" == this.certFile {
+			err = http.ListenAndServe(this.wsListenAddr, nil)
+		} else {
+			zplog.Infof("comAcceptor-websocket 使用 TLS。 key=%s, cert=%s", this.keyFile, this.certFile)
+			err = http.ListenAndServeTLS(this.wsListenAddr, this.certFile, this.keyFile, nil)
+		}
+
+		// 错误信息
+		if nil != err {
+			zplog.Fatalf("comAcceptor-websocket 启动失败。ip=%s，错误=%s", this.wsListenAddr, err)
+		}
+	}()
+}
+
+// 收到1个新的 websocket 连接
+func (this *comAcceptor) onNewWsConn(wsConn *websocket.Conn) {
+
 }
 
 // /////////////////////////////////////////////////////////////////////////////
